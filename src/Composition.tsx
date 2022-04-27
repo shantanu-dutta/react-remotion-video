d65f416c6ef6f727aa5e4e4a9e8811abe1847364
Shantanu Dutta <external.dutta_shantanu@allianz.com>
2022-04-27 21:40:19 +0530

    feat: create raindrops video

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,3 +1,24 @@
+import {useVideoConfig} from 'remotion';
+import {France} from './France';
+import {Rain} from './Rain';
+
 export const MyComposition = () => {
-	return null;
+	const {width, height, fps, durationInFrames} = useVideoConfig();
+
+	return (
+		<div
+			style={{
+				flex: 1,
+				textAlign: 'center',
+				fontSize: '7em',
+				background: 'black',
+				color: 'red',
+			}}
+		>
+			Hi Mom! 👋 I made a {width}x{height}px video that is{' '}
+			{durationInFrames / fps}s long.
+			<France />
+			<Rain />
+		</div>
+	);
 };
---
+++ src/Rain.tsx
@@ -0,0 +1,22 @@
+import React, {useMemo} from 'react';
+import {AbsoluteFill, random} from 'remotion';
+import {RainDrop} from './RainDrop';
+
+export const Rain: React.FC = () => {
+	const drops = useMemo(() => {
+		return new Array(300).fill(true).map((_, i) => {
+			const x = random('x' + i) * 100 + '%';
+			const delay = random('delay' + i) * 60;
+			const size = random('size' + i) + 0.3;
+			return {x, delay, size};
+		});
+	}, []);
+
+	return (
+		<AbsoluteFill>
+			{drops.map((d) => {
+				return <RainDrop x={d.x} size={d.size} delay={d.delay} />;
+			})}
+		</AbsoluteFill>
+	);
+};
